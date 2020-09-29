fa0864f6539fcb9ea9b1e2471be8126535ee9211
jhon alexander quiceno <jhon.quiceno@carvajal.com>
2020-09-29 18:54:51 -0500

    proyecto base, y servidor express

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'client/build')));
+
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+}
+
+app.listen(port, (error) => {
+	if (error) throw error;
+	console.log('Server running on port' + port);
+});
---
+++ src/client/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => <h1 className='title'>Prueba Front End Mercadolibre</h1>;
+
+export default App;
